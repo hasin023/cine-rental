8317f859342414ea0d208493c6c3141a51c0f162
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-01-28 20:12:58 +0600

    setup: set up for all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.25rem",
+      },
+      colors: {
+        primary: '#00D991'
+      },
+    },
   },
   plugins: [],
 }
